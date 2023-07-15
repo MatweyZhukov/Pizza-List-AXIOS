95a95fee3f8e2a2b95b412b5e6936e00517e8857
Zhukov Matwey <106314713+MatweyZhukov@users.noreply.github.com>
2023-07-16 00:45:17 +0300

    Add files via upload

---
+++ src/interfaces/types.ts
@@ -0,0 +1,36 @@
+//Global
+import { Dispatch, SetStateAction } from "react";
+
+type DeletePizzaFunc = (id: IPizzaItem["id"]) => void;
+
+export type RequestMethods = "GET" | "POST" | "DELETE";
+
+export interface IPizzaItem {
+  id: string;
+  title: string;
+  price: number;
+  img: string;
+}
+
+export interface IAddPizzaForm {
+  addPizza: (newPizza: IPizzaItem) => void;
+}
+
+export interface IDisplayPizzas {
+  pizzasList: IPizzaItem[];
+  deletePizza: DeletePizzaFunc;
+}
+
+export interface ISinglePizza extends Pick<IDisplayPizzas, "deletePizza"> {
+  pizza: IPizzaItem;
+}
+
+export interface IPizzaOptions {
+  pizzaImgOption: string;
+  setPizzaImgOption: Dispatch<SetStateAction<string>>;
+}
+
+export interface IPizzaOptionSingle {
+  id: string;
+  img: string;
+}
